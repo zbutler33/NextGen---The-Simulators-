cb1d12c824762f7e969e64186a0235f28181ac69
zprivf <fwolkeba@crimson.ua.edu>
2022-06-24 05:16:29 +0000

    New_Version

--- usgs.py
+++ usgs.py
@@ -2,12 +2,8 @@ from dataretrieval import nwis, utils, codes
 class USGS():
     def __init__(self):
         super(USGS, self).__init__()
-    def run_usgs(self, sites):
-        nwis.get_record(sites='03339000', service='peaks',start='1970-01-01',end='1990-01-01')
-        sites.streamflow = df['peak_va']
-        # ________________________________________________
-        sites.current_time_step += 1
-        sites.current_time      += sites.time_step_size
-
+    def run_usgs(sites,service,start,end):
+        site=nwis.get_record(sites=sites,service=service,start=start,end=end)
+        print(site['peak_va'])
         return
     
