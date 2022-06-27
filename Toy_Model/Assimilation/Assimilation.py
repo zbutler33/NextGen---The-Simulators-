ea6f9cfc03ebba3a2a2abdf61f8f356c8b9b1224
motasem-sulieman <motasem.abualqumboz@usu.edu>
2022-06-27 19:24:27 +0000

    average assimilation

--- Toy_Model/Assimilation/Assimilation.py
+++ Toy_Model/Assimilation/Assimilation.py
@@ -7,16 +7,20 @@ This framework will build in complication but start simple by 'nudging'
 
 
 class Assimilation():
-    def __init__(self):
+    
+    def __init__(self, peturb, cfe):
         super(Assimilation, self).__init__()
+        
         self.simulated = cfe
         self.observed = peturb
         self.assimilation = []
 
-    def run_assimilation(self, peturb, cfe): #two inputs of petrubed CFE and model CFE
+    def run_assimilation(self): #two inputs of petrubed CFE and model CFE
 
-        self.average = np.sum()
+        assimilation = (self.simulated + self.observed)/2
+        self.assimilation = assimilation
 
-        return
+        return self.assimilation
+    
         
 
