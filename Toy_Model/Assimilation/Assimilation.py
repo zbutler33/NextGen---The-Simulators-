c38a73a560c4796a7cd85363c7ff585817b8fc38
zbutler33 <butlerz@oregonstate.edu>
2022-06-28 13:05:40 -0500

    Trying to get BMI to work

--- Toy_Model/Assimilation/Assimilation.py
+++ Toy_Model/Assimilation/Assimilation.py
@@ -3,19 +3,22 @@ This code will assimilated CFE model data to CFE Peturbed data in our Sub-Region
 
 This framework will build in complication but start simple by 'nudging'
 
+Will build in complexity of data assimilation techniques
+
 '''
 
 
 class Assimilation():
-    
-    def __init__(self, obs, cfe):
+
+    #Put two inputs of petrubed CFE (USGS obs) and model CFE into assimilation definition
+    def __init__(self, obs, cfe):   
         super(Assimilation, self).__init__()
         
         self.simulated = cfe
         self.observed = obs
         self.assimilation = []
 
-    def run_assimilation(self): #two inputs of petrubed CFE and model CFE
+    def run_assimilation(self): 
 
         assimilation = (self.simulated + self.observed)/2
         self.assimilation = assimilation
