3053d4f44b2b7222c52e2399e5c1b1dc883170e7
motasem-sulieman <motasem.abualqumboz@usu.edu>
2022-06-30 15:43:35 +0000

    Runnig BMI trial

--- USGS/USGS_BMI_FT/cleaned_FT/usgs.py
+++ USGS/USGS_BMI_FT/cleaned_FT/usgs.py
@@ -24,18 +24,21 @@ class USGS():
         site_avg.reset_index(inplace=True) #reset index again to have datetime 
         site_avgflow = site_avg.iloc[:,[0,2]] #locates every row by the columns we want (date and flow)
         site_avgflow.columns = ['Date', 'Flow']
+        
         #check validity of extracted data
         site_avgflow.loc[site_avgflow['Flow'] >= 0, 'validity']=1 # if value positive, consider
         site_avgflow.loc[site_avgflow['Flow'] <0,'validity']=0 # if less than zero, not realistic
         site_avgflow.loc[site_avgflow['Flow'].isnull()==True, 'validity']=0 # if NaN not availible
+        
         #Output results to csv file
+        Flow = site_avgflow['Flow']
         site_avgflow.to_csv('USGS_'+str(sites)+'_obs_streamflow.csv', index=False)
         #site_avgflow.to_csv('USGS_streamflow_for_site_.csv', index=False)
         # to check if the code runs on the framework
-        print(site_avgflow) 
+        print(Flow) 
         print("USGS station ID",sites)
 
-        return
+        return Flow
         
 
 
